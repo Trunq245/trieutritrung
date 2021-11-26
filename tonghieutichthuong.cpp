315f6ecaf49f0fff154a45cfac11346ba74cbdd5
Trunq245 <trungtrung24503@gmail.com>
2021-11-26 08:55:13 +0700

    hehe

--- tonghieutichthuong.cpp
+++ tonghieutichthuong.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 main (){
-	int a, b, Tong, Hieu, Tich;
-	float Thuong;
+
+	float a, b, Tong, Hieu, Tich, Thuong;
 	printf ("nhap a:");
-	scanf ("%d", &a);
+	scanf ("%f", &a);
 	printf ("nhap b:");
-	scanf ("%d", &b);
-	Tong = a + b;
-	Hieu = a - b;
-	Tich = a * b;
-	Thuong = (float)a/(float)b;
-	printf ("a + b = %d\n", Tong);
-	printf ("a - b = %d\n", Hieu);
-	printf ("a * b = %d\n", Tich);
+	scanf ("%f", &b);
+	Tong = (float)a + (float)b;
+	Hieu = (float)a - (float)b;
+	Tich = (float)a * (float)b;
+	Thuong = (float)a / (float)b;
+	printf ("a + b = %.2f\n", Tong);
+	printf ("a - b = %.2f\n", Hieu);
+	printf ("a * b = %.2f\n", Tich);
 	printf ("a / b = %.2f\n", Thuong);
 	return 0;
 	}
